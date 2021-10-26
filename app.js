26c44efb4456993686e5aa8f1b27dc76808c37bc
JB-Tellez <jb.tellez@gmail.com>
2021-10-26 11:25:15 -0700

    adds initial files

---
+++ app.js
@@ -0,0 +1,17 @@
+let someData = prompt("Pick a number");
+
+someData = parseInt(someData);
+
+
+// oops, forget to run the code that sets someData
+
+
+let resultFromTypeOf = typeof (someData);
+
+console.log('resultFromTypeOf = ', resultFromTypeOf);
+
+let doubled = someData + someData;
+
+console.log('doubled', doubled);
+
+console.log(typeof (doubled));
